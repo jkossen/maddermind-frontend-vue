c1efbcea2647949ecb63cd4a877bfeb089b78a96
Jochem Kossen <jochem@jkossen.nl>
2022-02-27 22:05:04 +0100

    Configure axios base url using .env file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import {createRouter, createWebHashHistory} from "vue-router";
 import App from './App.vue'
 import './index.css'
+import axios from "axios";
 
 import StartScreen from "./components/StartScreen.vue"
 import GuessScreen from "./components/GuessScreen.vue"
+
+// @ts-ignore
+axios.defaults.baseURL = import.meta.env.VITE_API_BASEURL || 'http://localhost:12001/'
+axios.defaults.timeout = 1000
 
 const routes = [
     {
