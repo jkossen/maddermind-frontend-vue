dd37a796a175a68a993c8b3c1c838bbdedec93dc
Jochem Kossen <jochem@jkossen.nl>
2022-02-27 20:40:25 +0100

    Add StartScreen

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full h-screen flex items-center justify-around">
+    <span>
+    <router-link
+        to="/play"
+        class="bg-green-500 w-20 rounded-full h-20 text-center flex items-center justify-around font-bold cursor-pointer"
+    ></router-link>
+      </span>
+  </div>
+</template>
