5014be78dcccce856ca387e963243ff6e70f98e6
Jochem Kossen <jochem@jkossen.nl>
2022-03-07 15:50:02 +0100

    reformat with volar

--- src/components/GuessScreen.vue
+++ src/components/GuessScreen.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import {ref} from "vue"
+import { ref } from "vue"
 import axios from "axios"
-import {useRoute, useRouter} from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import NumberSquare from "./NumberSquare.vue";
 import ResultLine from "./ResultLine.vue";
 
@@ -82,66 +82,60 @@ const toggleNumbers = function () {
 </script>
 
 <template>
-    <span
-        class="mt-20 mb-10 w-10 h-10 mx-auto m-2 cursor-pointer text-center flex items-center justify-around font-bold rounded-full"
-        style="background-color: #5b2226; color: #fafafa"
-        @click="toggleNumbers()">
-    </span>
+  <span
+    class="mt-20 mb-10 w-10 h-10 mx-auto m-2 cursor-pointer text-center flex items-center justify-around font-bold rounded-full"
+    style="background-color: #5b2226; color: #fafafa"
+    @click="toggleNumbers()"
+  ></span>
   <div class="flex max-w-screen-sm mx-auto justify-around items-center mb-10">
     <NumberSquare
-        v-for="i in 10"
-        :key="i-1"
-        :num="i-1"
-        :codeLength="codeLength"
-        :class="'g-' + (i-1)"
-        :showNumbers="showNumbers"
-        @click="pickColor(i-1)"
+      v-for="i in 10"
+      :key="i - 1"
+      :num="i - 1"
+      :codeLength="codeLength"
+      :class="'g-' + (i - 1)"
+      :showNumbers="showNumbers"
+      @click="pickColor(i - 1)"
     />
   </div>
 
-  <div
-      class="flex max-w-screen-sm mx-auto justify-around items-center mb-10 py-2">
+  <div class="flex max-w-screen-sm mx-auto justify-around items-center mb-10 py-2">
     <div class="flex w-full mx-auto sm:justify-center justify-between">
-      <div v-for="i in codeLength" :key="i" class="w-10 h-15 flex flex-col"
-           :class="(codeLength > 4) ? 'sm:mx-2' : 'mx-2'"
+      <div
+        v-for="i in codeLength"
+        :key="i"
+        class="w-10 h-15 flex flex-col"
+        :class="(codeLength > 4) ? 'sm:mx-2' : 'mx-2'"
       >
         <NumberSquare
-            :num="newAttempt[i-1]"
-            :codeLength="codeLength"
-            :class="'g-' + newAttempt[i-1]"
-            :showNumbers="showNumbers"
+          :num="newAttempt[i - 1]"
+          :codeLength="codeLength"
+          :class="'g-' + newAttempt[i - 1]"
+          :showNumbers="showNumbers"
         />
 
-        <span
-            v-if="curCol == i-1"
-            class="w-10 h-2 mt-2"
-            style="background-color: #ee9b00;"
-        >
-        </span>
+        <span v-if="curCol == i - 1" class="w-10 h-2 mt-2" style="background-color: #ee9b00;"></span>
       </div>
 
-      <div class="w-20 h-10 flex justify-center"
-           :class="(codeLength > 4) ? 'sm:ml-2' : 'ml-2'">
+      <div class="w-20 h-10 flex justify-center" :class="(codeLength > 4) ? 'sm:ml-2' : 'ml-2'">
         <button
-            class="w-10 h-10 flex items-center justify-around font-bold bg-green-600 text-white rounded-full"
-            @click="checkAttempt">
-        </button>
+          class="w-10 h-10 flex items-center justify-around font-bold bg-green-600 text-white rounded-full"
+          @click="checkAttempt"
+        ></button>
       </div>
     </div>
   </div>
 
   <ResultLine
-      v-for="(a, i) in attempts"
-      :key="i"
-      :attempt="a"
-      :codeLength="codeLength"
-      :showNumbers="showNumbers"
+    v-for="(a, i) in attempts"
+    :key="i"
+    :attempt="a"
+    :codeLength="codeLength"
+    :showNumbers="showNumbers"
   />
-
 </template>
 
 <style>
-
 .g-10 {
   background-color: #d5d2ca;
 }
